1ecf41b53e14a38152408ef646139b6d61ad274d
mgwissa <michaelgwissa@gmail.com>
2021-10-06 22:58:16 -0400

    small style tweaks

--- my-app/pages/NecklacePrayerBeads.js
+++ my-app/pages/NecklacePrayerBeads.js
@@ -5,30 +5,34 @@ import { Center, Stack, Heading, Image, Link, InputGroup, InputLeftAddon, Input 
 const PrayerBeads = () => {
     return (
         <div>
-            <Center width="100%">
+            <Center marginBottom="10">
                 <Stack spacing="4">
                     <Heading>Necklace of Prayer Beads</Heading>
                     <Image boxSize="sm" class="prayer-beads-img" src="necklace_of_prayer_beads.jpeg" alt="Necklace of Prayer Beads" />
                     <Link href="https://www.dndbeyond.com/magic-items/necklace-of-prayer-beads" color="red.500">https://www.dndbeyond.com/magic-items/necklace-of-prayer-beads</Link>
+                </Stack>
+            </Center>
+            <Center>
+                <Stack spacing="4">
                     <InputGroup size="sm">
-                        <InputLeftAddon children="curing:" />
-                        <Input type="number" value="1" />
+                        <InputLeftAddon children="curing:" width="90px" />
+                        <Input type="number" placeholder="1" />
                     </InputGroup>
                     <InputGroup size="sm">
-                        <InputLeftAddon children="blessing:" />
-                        <Input type="number" value="1" />
+                        <InputLeftAddon children="blessing:" width="90px" />
+                        <Input type="number" placeholder="1" />
                     </InputGroup>
                     <InputGroup size="sm">
-                        <InputLeftAddon children="smiting:" />
-                        <Input type="number" value="1" />
+                        <InputLeftAddon children="smiting:" width="90px" />
+                        <Input type="number" placeholder="1" />
                     </InputGroup>
                     <InputGroup size="sm">
-                        <InputLeftAddon children="favor:" />
-                        <Input type="number" value="2" />
+                        <InputLeftAddon children="favor:" width="90px" />
+                        <Input type="number" placeholder="2" />
                     </InputGroup>
                     <InputGroup size="sm">
-                        <InputLeftAddon children="summons:" />
-                        <Input type="number" value="1" />
+                        <InputLeftAddon children="summons:" width="90px" />
+                        <Input type="number" placeholder="1" />
                     </InputGroup>
                 </Stack>
             </Center>
